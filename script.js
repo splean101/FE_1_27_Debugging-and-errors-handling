b3f932ff38aca3de087831f786ae8eb893fd7c33
splean101 <101splean@gmail.com>
2023-02-11 11:57:24 +0200

    fin

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//1.
+//--------------------1-----------------------------
 function count(expression) {
   function getValues(expression) {
     let values = [...expression];
@@ -23,22 +23,107 @@ function count(expression) {
 }
 
 function sum(values) {
-  return values[1] + values[2];
+  return +values[0] + +values[2];
 }
 function subtract(values) {
-  return values[1] - values[2];
+  return +values[0] - +values[2];
 }
 function multiply(values) {
-  return values[1] * values[2];
+  return +values[0] * +values[2];
 }
 function divide(values) {
-  return values[1] / values[2];
+  //   //--------------------2-----------------------------
+  if (+values[2] === 0) {
+    throw new Error('Error: Can`t devide by 0');
+  }
+  return +values[0] / +values[2];
 }
 
-console.log(count('5-2'));
+try {
+  count('5/0');
+} catch (err) {
+  console.log(err.message);
+  //--------------------2.1-----------------------------
+  console.log(0);
+}
 
-//2.
+//--------------------3-----------------------------
+function count1(expression) {
+  function getValues(expression) {
+    let values = [...expression];
+    return values;
+  }
+  let values;
+  try {
+    let data = getValues(expression);
+    values = data;
+  } catch (err) {
+    if (typeof expression !== 'string') {
+      alert('string, please');
+    }
+    if (err.message === 'expression is not iterable') {
+      alert('expression is not iterable');
+      return;
+    }
+    if (values === undefined) {
+      alert('values is not defined');
+    }
+    console.log(err.name + ': ' + err.message);
+    values = [0, '+', 0];
+  }
 
-//3.
+  switch (values[1]) {
+    case '+':
+      return showResult(sum1(values));
+    case '-':
+      return showResult(subtract1(values));
+    case '*':
+      return showResult(multiply1(values));
+    case '/':
+      return showResult(divide1(values));
+  }
+  function showResult(value) {
+    return value;
+  }
+}
 
-//4.
+function sum1(values) {
+  return +values[0] + +values[2];
+}
+function subtract1(values) {
+  return +values[0] - +values[2];
+}
+function multiply1(values) {
+  return +values[0] * +values[2];
+}
+function divide1(values) {
+  return +values[0] / +values[2];
+}
+
+console.log(count1('5+2'));
+
+//--------------------4-----------------------------
+
+function MakeUsers(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const user = new MakeUsers('Mike');
+function showMovie(user) {
+  try {
+    if (user.age === undefined) {
+      throw new Error();
+    }
+  } catch (error) {
+    user.age = prompt('your age please');
+    console.log(user.age);
+  }
+  if (user.age >= 18) {
+    console.log('You can watch this movie');
+  } else if (user.age > 0) {
+    console.log('Sorry, you are too young');
+  }
+}
+
+showMovie(user);
